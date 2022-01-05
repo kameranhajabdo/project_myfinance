bb886fa01f3da7adff01ff4df239568079fef3ec
kameranhajabdo <kameranhajabdo1@gmail.com>
2022-01-05 20:35:39 +0200

    add homework3 and for lists

--- courses/join_words.py
+++ courses/join_words.py
@@ -5,3 +5,4 @@ args = sys.argv[1:]
 strings = [ x for x in args if not x.isnumeric()]
 
 print(strings)
+
---
+++ homework_solutions/christmas_form.py
@@ -0,0 +1,17 @@
+list1 = ["a", "b", "c"]
+list2 = ["c", "a", "b"]
+
+print(list1[0], list2[0])
+print(list1 == list2)
+
+d = {"a":0, "b":0, "c":0}
+print(len(d))
+
+d2 = {"a":1, "b":2, "c":4, "d":8}
+print(d2.items())
+print(d2.values())
+print(d2.keys())
+
+new_list = [ x for x in ["1", "2", "3", "a" "b"] if not x.isnumeric()]
+print(new_list)
+
